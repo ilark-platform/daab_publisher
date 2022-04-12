38cb4302be83bc845a1129c48f5f747825ac6021
vue_ys <lys@ilark.co.kr>
2022-04-12 10:37:57 +0900

    랜딩 페이지 리셋 테스트

---
+++ html/daabLanding202204/js/function.js
@@ -0,0 +1,31 @@
+$(function() {
+
+	$(window).on('load', function () {
+    	AOS.init({
+			once: true
+		});
+	});
+
+	$(window).on('scroll', function () {
+        if ($(this).scrollTop() > 0) {
+			$('#header').addClass('fixed');
+		} else {
+			$('#header').removeClass('fixed');
+		}
+	});
+
+	$(document).ready(function(){
+		$('#main_visual').addClass('active');
+		$('#main_visual .bg_box').delay(600).animate({'z-index':'0'}, function(){
+			$(this).hide();
+		})
+	});
+
+	function appHeight() {
+		var doc = document.documentElement;
+		doc.style.setProperty('--app-height', $(window).innerHeight() + 'px');
+	}
+	window.addEventListener('resize', appHeight);
+	appHeight();
+	
+});
